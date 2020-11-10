4202a1d040495957c8aec7a7e654705c4534f33c
shivaraj loni <shivarajbloni@gmail.com>
2020-11-10 20:27:06 +0530

    Max subarray sum- w/ different approachs-

---
+++ array_probs/max_sub-array-sum.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<climits>
+        // using Brutte force - O(1) space, "Poor solution"
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++) {
+        cin>>arr[i];
+    }
+
+    int max_sum = INT_MIN;
+    for(int i=0; i<n; i++) {
+        for(int j=i; j<n; j++)
+        {
+            int sum=0;
+            for(int k=i; k<=j; k++) {
+              sum += arr[k];
+              //  cout<<arr[k]<<" ";
+            } 
+            // cout<<endl;
+            max_sum=max(max_sum, sum);
+        }
+    }
+    cout<<max_sum<<endl;
+
+    return 0;
+}
