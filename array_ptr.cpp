86362ec658f9ba84e78cddfed5784bf351a24615
shivaraj loni <shivarajbloni@gmail.com>
2020-11-14 22:36:27 +0530

    pointers and arrays

---
+++ array_ptr.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    // int a=10;
+    // int* aptr = &a;
+
+    // cout<<*aptr<<endl;
+    // *aptr=20;
+    // cout<<a<<endl;
+
+    int arr[]={10,20,30};
+    cout<<*arr<<endl;
+
+    int *ptr = arr;
+    for(int i=0; i<3; i++) {
+        cout<<*ptr<<endl;
+        ptr++;
+    
+    // cout<<*arr<<endl;
+        //    arr++;   // illegal 
+    }
+
+    return 0;
+}
