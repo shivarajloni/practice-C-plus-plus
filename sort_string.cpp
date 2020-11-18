aeec94474be09eebe0c03fc3dd2522a1a97713a9
Shivaraj Loni <42679657+shivarajloni@users.noreply.github.com>
2020-11-18 19:40:39 +0530

    sort in a string using sort()

---
+++ sort_string.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+int main() 
+{
+    string s1 = "sdnljdshdsjndjocnos";
+
+    sort(s1.begin(), s1.end());
+
+    cout << s1 << endl;
+
+return 0;
+}
